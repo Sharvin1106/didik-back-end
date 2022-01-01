3a8903b1183324fca97ab4c215386ea5331ef572
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2022-01-02 00:40:46 +0800

    Added support for dotenv in heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv/config");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-
-app.use(express.json())
+app.use(express.json());
 //Import Routes
 
 const servicesRoute = require("./routes/services");
